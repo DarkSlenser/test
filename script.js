96971ad9ced4f3393296f7ab90847051d949efe7
Slenser <mrwer12@gmail.com>
2019-05-05 21:52:49 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-//'use strict';
 
-a = 5;
-console.log(a);
+
+var
+    money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD"),
+
+
+    appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses:"",
+    income: {},
+    savings: false
+
+};
+
+    
+
+
+for (let i = 0; i<2;i++) {
+    let
+        a = prompt("Введите обязательную статью расходов в этом месяце",""),
+        b = prompt("Во сколько обойдется?","");
+
+    if( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null 
+    && a != "" && b!= "" && a.length < 50) {
+    console.log("Верно");
+    appData.expenses[a] = b;
+    } else {
+
+    }
+};  
+
+appData.moneyPerDay = appData.budget / 30;  
+alert("Бюджет на 1 день " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Миннимальный уровень достатка ");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 200) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
